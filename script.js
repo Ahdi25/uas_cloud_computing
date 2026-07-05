c71cb4eedb5c4f69b28e96a6688839fd4e7f5ac1
Ahdi25 <ahdiyurikza2525@gmail.com>
2026-07-05 11:15:14 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showMessage() {
+
+    const hasil = document.getElementById("hasil");
+
+    hasil.innerHTML =
+    "Website ini dibuat sebagai media sederhana untuk memenuhi tugas UAS mata kuliah Cloud Computing menggunakan HTML, CSS, dan JavaScript.";
+}
